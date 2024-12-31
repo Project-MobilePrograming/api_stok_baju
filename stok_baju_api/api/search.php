7e8684d1ad2a53be414c1502ea6f2e6fbfcc895d
Borbobb <madeadisujaya23@gmail.com>
2024-12-31 18:45:41 +0800

    Fix CRUD

--- stok_baju_api/api/search.php
+++ stok_baju_api/api/search.php
@@ -7,8 +7,8 @@ $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
 
 $query = "SELECT baju.id_baju, baju.nama_baju, jenis_baju.nama_jenis_baju, ukuran_baju.ukuran_baju, baju.harga, baju.stok 
           FROM baju 
-          INNER JOIN jenis_baju ON baju.id_jenis_baju = jenis_baju.id_jenis_baju 
-          INNER JOIN ukuran_baju ON baju.id_ukuran = ukuran_baju.id_ukuran 
+          INNER JOIN jenis_baju ON baju.id_jenis_baju = jenis_baju_id.id_jenis_baju 
+          INNER JOIN ukuran_baju ON baju.id_ukuran_baju = ukuran_baju_id.id_ukuran_baju
           WHERE baju.nama_baju LIKE :keyword";
 $stmt = $conn->prepare($query);
 $stmt->bindValue(":keyword", "%$keyword%");
