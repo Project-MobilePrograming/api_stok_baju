971c0bbdb236ffb9496688d9955095e3a2ae9649
Borbobb <madeadisujaya23@gmail.com>
2025-01-02 15:30:32 +0800

    Fix Upload Gambar

--- stok_baju_api/api/baju/create.php
+++ stok_baju_api/api/baju/create.php
@@ -2,6 +2,26 @@
 header("Content-Type: application/json");
 require_once '../../db/connection.php';
 
+$data = json_decode(file_get_contents("php://input"));
+
+if (!empty($data->nama_baju) && !empty($data->id_jenis_baju) && !empty($data->id_ukuran_baju) && !empty($data->harga) && !empty($data->stok)) {
+    $query = "INSERT INTO baju (nama_baju, id_jenis_baju, id_ukuran_baju, harga, stok) VALUES (:nama_baju, :id_jenis_baju, :id_ukuran_baju, :harga, :stok)";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":nama_baju", $data->nama_baju);
+    $stmt->bindParam(":id_jenis_baju", $data->id_jenis_baju);
+    $stmt->bindParam(":id_ukuran_baju", $data->id_ukuran_baju);
+    $stmt->bindParam(":harga", $data->harga);
+    $stmt->bindParam(":stok", $data->stok);
+
+    if ($stmt->execute()) {
+        echo json_encode(["message" => "Baju berhasil ditambahkan."]);
+    } else {
+        echo json_encode(["message" => "Gagal menambahkan baju."]);
+    }
+} else {
+    echo json_encode(["message" => "Data tidak lengkap."]);
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
         // Validasi ID baju yang dikirim
@@ -45,23 +65,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo json_encode(["success" => false, "error" => "Metode request tidak valid."]);
 }
 
-$data = json_decode(file_get_contents("php://input"));
-
-if (!empty($data->nama_baju) && !empty($data->id_jenis_baju) && !empty($data->id_ukuran_baju) && !empty($data->harga) && !empty($data->stok)) {
-    $query = "INSERT INTO baju (nama_baju, id_jenis_baju, id_ukuran_baju, harga, stok) VALUES (:nama_baju, :id_jenis_baju, :id_ukuran_baju, :harga, :stok)";
-    $stmt = $conn->prepare($query);
-    $stmt->bindParam(":nama_baju", $data->nama_baju);
-    $stmt->bindParam(":id_jenis_baju", $data->id_jenis_baju);
-    $stmt->bindParam(":id_ukuran_baju", $data->id_ukuran_baju);
-    $stmt->bindParam(":harga", $data->harga);
-    $stmt->bindParam(":stok", $data->stok);
-
-    if ($stmt->execute()) {
-        echo json_encode(["message" => "Baju berhasil ditambahkan."]);
-    } else {
-        echo json_encode(["message" => "Gagal menambahkan baju."]);
-    }
-} else {
-    echo json_encode(["message" => "Data tidak lengkap."]);
-}
 ?>
